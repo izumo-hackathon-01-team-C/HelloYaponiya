8598f352d066d6691d17c17b2b9e62e9ead5fc9a
Savva Frolenko <s.frolenko@betby.com>
2024-02-25 16:50:04 +0700

    Add tortoise orm

--- src/database/models.py
+++ src/database/models.py
@@ -7,40 +7,56 @@ class CategoryDBModel(Model):
 
     name = fields.CharField(max_length=100)
 
+    class Meta:
+        table = "categories"
+
 
 class SectionDBModel(Model):
     id = fields.IntField(pk=True)
 
     name = fields.CharField(max_length=100)
-    category_id = fields.ForeignKeyField('CategoryDBModel', related_name='section')
+    category = fields.ForeignKeyField('models.CategoryDBModel', related_name='section')
+
+    class Meta:
+        table = "sections"
 
 
 class FormTemplateDBModel(Model):
     id = fields.IntField(pk=True)
 
     name = fields.CharField(max_length=200)
-    description = fields.TextField()
     excel_data = fields.data.BinaryField()
     markup_data = fields.data.JSONField()
+
+    class Meta:
+        table = "form_templates"
 
 
 class FormMetadataDBModel(Model):
     id = fields.IntField(pk=True)
 
-    form_id = fields.ForeignKeyField('FormTemplatesDBModel', related_name='form_metadata')
-    category_id = fields.ForeignKeyField('CategoryDBModel', related_name='form_metadata')
-    section_id = fields.ForeignKeyField('SectionDBModel', related_name='form_metadata')
+    description = fields.TextField()
+
+    form = fields.ForeignKeyField('models.FormTemplateDBModel', related_name='form_metadata')
+    category = fields.ForeignKeyField('models.CategoryDBModel', related_name='form_metadata')
+    section = fields.ForeignKeyField('models.SectionDBModel', related_name='form_metadata')
 
     created_at = fields.DatetimeField(auto_now_add=True)
     updated_at = fields.DatetimeField(auto_now=True)
+
+    class Meta:
+        table = "form_metadata"
 
 
 class LocalizationDBModel(Model):
     id = fields.IntField(pk=True)
 
-    form_id = fields.ForeignKeyField('FormDBModel', related_name='form_localizations')
+    form = fields.ForeignKeyField('models.FormTemplateDBModel', related_name='form_localizations')
     name = fields.CharField(max_length=1000)
     lang = fields.CharField(max_length=10)
     value = fields.CharField(max_length=1000)
+
+    class Meta:
+        table = "form_localizations"
 
 
