b7fb76bf77a1f437914bf47b1078b1688566c4f6
Savva Frolenko <s.frolenko@betby.com>
2024-02-25 15:07:13 +0700

    Add database models

---
+++ src/database/models.py
@@ -0,0 +1,44 @@
+from tortoise.models import Model
+from tortoise import fields
+
+
+class CategoryDBModel(Model):
+    id = fields.IntField(pk=True)
+    name = fields.CharField(max_length=100)
+
+
+class SectionDBModel(Model):
+    id = fields.IntField(pk=True)
+    name = fields.CharField(max_length=100)
+    category_id = fields.ForeignKeyField('CategoryDBModel', related_name='section')
+
+
+class FormTemplatesDBModel(Model):
+    id = fields.IntField(pk=True)
+
+    name = fields.CharField(max_length=200)
+    description = fields.TextField()
+    excel_data = fields.data.BinaryField()
+    markup_data = fields.data.JSONField()
+
+
+class FormMetadataDBModel(Model):
+    id = fields.IntField(pk=True)
+
+    form_id = fields.ForeignKeyField('FormTemplatesDBModel', related_name='form_metadata')
+    category_id = fields.ForeignKeyField('CategoryDBModel', related_name='form_metadata')
+    section_id = fields.ForeignKeyField('SectionDBModel', related_name='form_metadata')
+
+    created_at = fields.DatetimeField(auto_now_add=True)
+    updated_at = fields.DatetimeField(auto_now=True)
+
+
+class LocalizationsDBModel(Model):
+    id = fields.IntField(pk=True)
+
+    form_id = fields.ForeignKeyField('FormDBModel', related_name='form_localizations')
+    name = fields.CharField(max_length=1000)
+    lang = fields.CharField(max_length=10)
+    value = fields.CharField(max_length=1000)
+
+
