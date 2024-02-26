08960b3a52d579278991eab2bfd5e10df7d592c3
Savva Frolenko <s.frolenko@betby.com>
2024-02-26 14:01:22 +0700

    Make web facade better

---
+++ src/models/enums.py
@@ -0,0 +1,11 @@
+from enum import StrEnum
+
+
+class LanguageEnum(StrEnum):
+    JAPANESE = 'ja'
+    ENGLISH = 'en'
+    RUSSIAN = 'ru'
+    SPANISH = 'es'
+    PORTUGUESE = 'pt-BR'
+    CHINESE_SIMPLE = 'zh-Hans'
+    FRENCH = 'fr'
